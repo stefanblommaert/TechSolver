302d48cbd99637c735358ccef8d137eb1005e0c3
stefanblommaert <Stefan@MacBook-Pro-van-Stefan.local>
2016-04-11 14:21:08 +0200

    improved login.php added member.php & register.php

--- bin/login.php
+++ bin/login.php
@@ -1,3 +1,33 @@
 <?php
-    $host="localhost";
-?>
+
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password=""; // Mysql password 
+$db_name="test"; // Database name 
+$tbl_name="members"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+
+// username and password sent from form 
+$username=$_POST['username']; 
+$password=$_POST['password']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+$sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $username and $password, table row must be 1 row
+if($count==1){
+    session_start();
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+}
---
+++ bin/member.php
@@ -0,0 +1,6 @@
+session_start();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+} else {
+    echo "Please log in first to see this page.";
+}
