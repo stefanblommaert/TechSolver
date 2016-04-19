d97d568dd6f81fae2c6c14b3152715ac98370af9
stefanblommaert <Stefan@MacBook-Pro-van-Stefan.local>
2016-04-19 21:57:39 +0200

    Added fb login (works only in the log for now)

--- master/script.js
+++ master/script.js
@@ -27,18 +27,55 @@ app.config(function($routeProvider){
                 templateUrl: 'other.html'
           })
           .when('/login',{
+<<<<<<< Updated upstream
                 templateUrl: 'login.html'
           })
           .when('/register',{
           		templateUrl: 'register.html'
+=======
+          		templateUrl: 'login.html'
+          		
+>>>>>>> Stashed changes
           });
 
-
+			
+			
 });
-
 
 app.controller('cfgController',function($scope){
 
-      $scope.message="Hello world";
+      $scope.FBLogin = function(){
+      	FB.login(function(response) {
+    	if (response.authResponse) {
+     		console.log('Welcome!  Fetching your information.... ');
+     		FB.api('/me', function(response) {
+       		console.log('Good to see you, ' + response.name + '.');
+       		console.log(response);
+       		
+       		var accessToken = FB.getAuthResponse();
+       		console.log(accessToken);
+     	});
+     	
+    	} else {
+     		console.log('User cancelled login or did not fully authorize.');
+    	}
+}, {scope: 'email'});
+      };
 
 });
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1526991634270394',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
