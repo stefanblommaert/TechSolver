7b3b566703e058610fb2801ce44b3bc02590cca8
stefanblommaert <Stefan@MacBook-Pro-van-Stefan.local>
2016-05-23 15:16:54 +0200

    Chat moved to master

--- master/script.js
+++ master/script.js
@@ -46,7 +46,7 @@ app.config(function($routeProvider){
           })
           .when('/chat',{
           	
-          		templateUrl: 'chat/chat1.html',
+          		templateUrl: 'chat.html',
           		controller: 'chatController',
           		resolve: {
       				accessChat: ['$q', function($q) {
@@ -195,5 +195,25 @@ app.controller('cfgController',function($scope, $http, $location){
      
 
 });
+
+var myDataRef = new Firebase('https://resplendent-inferno-9134.firebaseio.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      //checken wanneer nieuwe chat wordt toegevoegd
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      //message toevoegen aan chatdiv
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      }
    
    
